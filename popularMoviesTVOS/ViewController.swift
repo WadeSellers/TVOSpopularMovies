678dae344677b70b5bd796b25faf9fec9bd568ed
Wade Sellers <Waderoni@gmail.com>
2015-10-08 18:44:16 -0600

    Initial Commit

---
+++ popularMoviesTVOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  popularMoviesTVOS
+//
+//  Created by Wade Sellers on 10/8/15.
+//  Copyright © 2015 Apps by Wade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
